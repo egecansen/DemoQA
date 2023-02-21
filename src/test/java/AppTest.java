5a88a0580b483b3edfe91dc03a1988e9d8144fdb
egecansen <118941751+egecansen@users.noreply.github.com>
2023-02-21 22:49:53 +0300

    Slider interaction

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/java/features"},
+        plugin = {"json:target/reports/Cucumber.json", "html:target/reports/Cucumber.html"},
         glue = {"steps"},
         publish = true
 )
